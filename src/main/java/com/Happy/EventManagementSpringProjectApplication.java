3cd3f3c74c1aae54c594191683809155e2f4103c
SHAKTHI SRAVANTH <shakthisravanth@SHAKTHIs-MacBook-Air.local>
2024-04-06 19:00:15 +0530

    Project Completed

---
+++ src/main/java/com/Happy/EventManagementSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Happy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementSpringProjectApplication.class, args);
+	}
+
+}
